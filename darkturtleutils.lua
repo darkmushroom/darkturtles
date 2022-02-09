ca0a69957757e576d0ac01375788b45fb19da0e0
Alexander Mensing <alex.mensing@cleverfew.co>
2022-02-09 13:23:13 -0500

    I wasn't building the module right

--- darkturtleutils.lua
+++ darkturtleutils.lua
@@ -1,6 +1,6 @@
 local darkturtleutils = {}
 
-local function grabSome(block)
+local function darkturtleutils.grabSome(block)
   -- short circuit if item is already selected
   if turtle.getItemCount() > 0 then
     if turtle.getItemDetail().name == block then
@@ -24,14 +24,14 @@ local function grabSome(block)
 end
 
 
-local function fuelCheck()
+local function darkturtleutils.fuelCheck()
   if turtle.getFuelLevel() < 80 then
     grabSome(fuel)
     turtle.refuel(1)
   end
 end
 
-local function clearScreen()
+local function darkturtleutils.clearScreen()
   term.clear()
   term.setCursorPos(1,1)
 end
